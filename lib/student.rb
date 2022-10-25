28d6f0366aeda2da4c6fe202f3f57ae80849c189
abraham <apolichenco@gmail.com>
2022-10-25 13:32:10 -0400

    commit tree e16c77a87cb5311f2c72d99bb39880ba76f99514

---
+++ lib/student.rb
@@ -0,0 +1,15 @@
+require 'pry'
+
+class Student
+
+    def hello
+        # binding.pry
+        puts "Hey there! I'm so excited to learn stuff."
+    end
+
+    def raise_hand
+        # binding.pry
+        puts "Pick me!"
+    end  
+
+end
